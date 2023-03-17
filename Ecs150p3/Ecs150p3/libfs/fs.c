a632236bc96f1ec1a6c280a73079cdfca1ec57c4
LinkaiShao <linkai.k.shao@gmail.com>
2023-03-16 18:46:39 -0700

    Update fs.c

--- Ecs150p3/Ecs150p3/libfs/fs.c
+++ Ecs150p3/Ecs150p3/libfs/fs.c
@@ -58,7 +58,7 @@ int fs_mount(const char *diskname) {
 	// need to match the fats and put them into fat_representation
 	int block_track = 1;
 	int offset = 0;
-	for(block_track; block_track < 1 + first_block.Fat_Blocks; block_track++) {
+	for(block_track = 1; block_track < 1 + first_block.Fat_Blocks; block_track++) {
 		if(block_read(block_track,&fat_representation[offset]) == -1) {
 			return -1;
 		}
@@ -103,7 +103,7 @@ int fs_info(void) {
 	// fat blocks
 	printf("fat_blk_count=%d\n", first_block.Fat_Blocks);
 	// which block is the rdir
-	printf("rdir location=%d\n", first_block.Root_Dir);
+	printf("rdir_blk=%d\n", first_block.Root_Dir);
 	// where is data start
 	printf("data_blk=%d\n",first_block.Data_Start);
 	// how many data blocks there are
@@ -527,7 +527,7 @@ int fs_read(int fd, void *buf, size_t count){
 	void* dirty_block = malloc(BLOCK_SIZE*sizeof(char));
 	block_read(current_fat,dirty_block);
 	// if whatever is left over of the dirty block is greater than count
-	if((int)count < BLOCK_SIZE - offset_left){
+	if((int)count <= BLOCK_SIZE - offset_left){
 		// 2 cases
 		// 1: whatever is left of the block we want
 		// 2: the block is not fully written
